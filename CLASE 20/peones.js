d311a03c1ce7da1a893c08843b8553927027dd10
daniel <danielejm1304@gmail.com>
2021-05-25 16:12:36 -0500

    Restauración archivos eliminados

--- CLASE 20/peones.js
+++ CLASE 20/peones.js
@@ -25,30 +25,3 @@ function dibujarPeon (x, y, tam, color) {
     ctx.stroke ()
     ctx.fill ()
     }
-
-let x1 = 30;
-let y1 = 150;
-let vX1 = 40; 
-let x2 = 200;
-let y2 = 30;
-let vY2 = 200;
-let x3 = 370;
-let y3 = 30;
-let v3 = 100;
-let dT = 5; 
-
-function Animar(){
-    ctx.clearRect(0,0, 800, 800);
-    let dX1 = (dT/1000) * vX1;
-    x1 = x1 + dX1;
-    dibujarPeon(x1, y1, 30,"LightGreen");
-    let dY2 = (dT/1000) * vY2;
-    y2 = y2 + dY2;
-    dibujarPeon(x2, y2, 30,"SpringGreen");
-    let d3 = (dT/1000) * v3;
-    y3 = y3 + d3;
-    x3 = x3 - d3;
-    dibujarPeon(x3, y3, 30,"MediumSpringGreen");
-
-}
-window.setInterval(Animar, dT);
